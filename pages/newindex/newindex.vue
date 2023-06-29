bd4bcf822668348069b30296ff55fb323e50d9ce
JasonTu <874572491@qq.com>
2023-06-29 22:19:33 +0800

    修改底部tabbar，新增新首页页面

---
+++ pages/newindex/newindex.vue
@@ -0,0 +1,27 @@
+<template>
+	<view class="body">
+		<!-- 轮播图区域 -->
+		<view class="swiper_form"></view>
+	</view>
+</template>
+
+<script>
+	export default{
+		
+	}
+</script>
+
+<style lang="scss" scoped>
+	.body{
+		width: 100%;
+		height: 100%;
+		border: 1px solid red;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		.swiper_form{
+			width: 100%;
+			height: 844rpx;
+		}
+	}
+</style>
